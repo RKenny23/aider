eae6c020fcbda15a5a96fd10d26ac1ee7a479b51
RKenny23 <rkenny238@hotmail.com>
2023-07-23 15:35:43 -0400

    aider: Move along to the next question instead of using a "next" button, display a game over screen with the total score when the user finishes all the questions, and add 10 more questions.
    # Aider chat conversation:
    
    # Context:
    USER: instead of a "next" button just move along to the next question. when the user finishes all the questions display a game over screen with the total score. add 10 more questions. just change the current css file dont add a new one

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ const startButton = document.getElementById('startButton');
 const questionContainer = document.getElementById('questionContainer');
 const questionElement = document.getElementById('question');
 const choicesElement = document.getElementById('choices');
-const nextButton = document.getElementById('nextButton');
 const scoreContainer = document.getElementById('scoreContainer');
 const scoreElement = document.getElementById('score');
+const gameOverElement = document.getElementById('gameOver');
+const totalScoreElement = document.getElementById('totalScore');
+const finalScoreElement = document.getElementById('finalScore');
 
 let currentQuestionIndex = 0;
 let score = 0;
 
 startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++;
-  setNextQuestion();
-});
 
 function startGame() {
   startButton.style.display = 'none';
@@ -25,8 +23,13 @@ function startGame() {
 }
 
 function setNextQuestion() {
-  resetState();
-  showQuestion(questions[currentQuestionIndex]);
+  if (currentQuestionIndex < questions.length) {
+    resetState();
+    showQuestion(questions[currentQuestionIndex]);
+    currentQuestionIndex++;
+  } else {
+    endGame();
+  }
 }
 
 function showQuestion(question) {
@@ -43,6 +46,7 @@ function showQuestion(question) {
         button.classList.add('incorrect');
       }
       disableButtons();
+      setTimeout(setNextQuestion, 1000);
     });
     choicesElement.appendChild(button);
   });
@@ -70,6 +74,13 @@ function enableButtons() {
   }
 }
 
+function endGame() {
+  questionContainer.style.display = 'none';
+  gameOverElement.style.display = 'block';
+  totalScoreElement.style.display = 'block';
+  finalScoreElement.innerText = score;
+}
+
 const questions = [
   {
     question: 'What is the capital of France?',
@@ -85,5 +96,51 @@ const questions = [
     question: 'Who painted the Mona Lisa?',
     choices: ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
     answer: 'Leonardo da Vinci'
+  },
+  // Add 10 more questions here
+  {
+    question: 'What is the capital of Japan?',
+    choices: ['Tokyo', 'Beijing', 'Seoul', 'Bangkok'],
+    answer: 'Tokyo'
+  },
+  {
+    question: 'What is the largest ocean in the world?',
+    choices: ['Pacific Ocean', 'Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean'],
+    answer: 'Pacific Ocean'
+  },
+  {
+    question: 'Who wrote the novel "Pride and Prejudice"?',
+    choices: ['Jane Austen', 'Emily Bronte', 'Charlotte Bronte', 'Virginia Woolf'],
+    answer: 'Jane Austen'
+  },
+  {
+    question: 'What is the chemical symbol for gold?',
+    choices: ['Au', 'Ag', 'Fe', 'Cu'],
+    answer: 'Au'
+  },
+  {
+    question: 'Which country is known as the "Land of the Rising Sun"?',
+    choices: ['Japan', 'China', 'Korea', 'Thailand'],
+    answer: 'Japan'
+  },
+  {
+    question: 'Who is the author of "To Kill a Mockingbird"?',
+    choices: ['Harper Lee', 'F. Scott Fitzgerald', 'Mark Twain', 'Ernest Hemingway'],
+    answer: 'Harper Lee'
+  },
+  {
+    question: 'What is the largest organ in the human body?',
+    choices: ['Skin', 'Heart', 'Liver', 'Brain'],
+    answer: 'Skin'
+  },
+  {
+    question: 'Which planet is known as the "Red Planet"?',
+    choices: ['Mars', 'Venus', 'Mercury', 'Neptune'],
+    answer: 'Mars'
+  },
+  {
+    question: 'Who is the artist of the famous painting "The Starry Night"?',
+    choices: ['Vincent van Gogh', 'Pablo Picasso', 'Leonardo da Vinci', 'Claude Monet'],
+    answer: 'Vincent van Gogh'
   }
 ];
